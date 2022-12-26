75e74408f47a554e0b7c29a61c1a12da0abd58cb
Kholiq <kholiqcode@gmail.com>
2022-12-27 05:31:24 +0700

    feat: add pgxpool

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,51 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	common_utils "github.com/kholiqcode/go-common/utils"
+	"github.com/pkg/errors"
+)
+
+const (
+	maxConn           = 50
+	healthCheckPeriod = 1 * time.Minute
+	maxConnIdleTime   = 1 * time.Minute
+	maxConnLifetime   = 3 * time.Minute
+	minConns          = 10
+	lazyConnect       = false
+)
+
+// NewPgxConn pool
+func NewPgxConn(cfg common_utils.Database) (*pgxpool.Pool, error) {
+	ctx := context.Background()
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.User,
+		cfg.Name,
+		cfg.Password,
+	)
+
+	poolCfg, err := pgxpool.ParseConfig(dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	poolCfg.MaxConns = maxConn
+	poolCfg.HealthCheckPeriod = healthCheckPeriod
+	poolCfg.MaxConnIdleTime = maxConnIdleTime
+	poolCfg.MaxConnLifetime = maxConnLifetime
+	poolCfg.MinConns = minConns
+	poolCfg.LazyConnect = lazyConnect
+
+	connPool, err := pgxpool.ConnectConfig(ctx, poolCfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "pgx.ConnectConfig")
+	}
+
+	return connPool, nil
+}
